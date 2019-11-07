d1b2298ee4be5371dbf7229d8afb9056700cd58b
jonny <jonathanhanley1234@gmail.com>
2019-11-07 22:45:49 +0000

    Fixed PEP 8 formatting Removed redundant lines

--- run_bot.py
+++ run_bot.py
@@ -1,67 +1,49 @@
-import ssl
 import re
 import requests
-import urllib.request
-import shutil
-import os
 import discord
 from discord.ext import commands
-import asyncio
-from discord.utils import get
+
 TOKEN = 'TOKEN HERE'
-
-def stash(name):
-    name=name.replace(' ','+')
-    searchPage=requests.get('https://stashpedia.com/search?terms=%s'%(name))
-    searchPage=searchPage.text
-    link=re.findall('<a class="fill-height" href="(.*?)"', searchPage)[0]
-    link='https://stashpedia.com'+link
-    mainPage=requests.get(link)
-    mainPage=mainPage.text
-    number='#'+str(re.findall('<H5>#(.*?)<', mainPage)[0])
-    stashPrice=re.findall('<span class="gridValue">(.*?)<', searchPage)[0]
-    title=re.findall('<H4 class="toUpperCase">(.*?)<', searchPage)[0]
-    imageLink='https://stashpedia.com'+re.findall('img-responsive gridImage" src="(.*?)"', searchPage)[0]
-    return (stashPrice,title,number,imageLink,link)
-
-def ebay(search):
-    link='https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313.TR12.TRC2.A0.H0.XTEST.TRS0&_nkw=%s&_sacat=0&LH_TitleDesc=0&_osacat=0&_odkw=%%23101'%(search)
-    print(link)
-    searchPage=requests.get(link)
-    searchPage=searchPage.text
-    title = re.findall('<h3 class="s-item__title" role="text">(.*?)<', searchPage)[0]
-    price = re.findall('<span class="s-item__price">(.*?)<', searchPage)[0]
-    return price
-    pass
-
-def main(name):
-    stashPrice,title,number,imageLink,link=stash(name)
-    ebaySearch=(title+' '+number).replace(' ','+').replace('#','%23')
-    ebayPrice=ebay(ebaySearch)
-
-    mess='''**%s**
-*Ebay Price*
-%s
-*Stashpedia Price*
-%s
-*Link*
-<%s>
-'''%(title,ebayPrice,stashPrice,link)
-    embed = discord.Embed(title=title, url=link,description="", color=0x00ff00)
-    embed.add_field(name="Ebay Price", value=ebayPrice, inline=False)
-    embed.add_field(name="Stashpedia Price", value=stashPrice, inline=False)
-    embed.set_image(url=imageLink)
-
-    return (embed)
-'''
-esl._create_default_https_context = ssl._create_unverified_context
-fp = requests.get('https://twitter.com/BotBroker')
-fp = fp.text
-print(fp)
-'''
-
 description = ''''''
 bot = commands.Bot(command_prefix='?', description=description)
+
+
+def stash(name):
+    name = name.replace(' ', '+')
+    search_page = requests.get('https://stashpedia.com/search?terms=%s' % name)
+    search_page = search_page.text
+    link = re.findall('<a class="fill-height" href="(.*?)"', search_page)[0]
+    link = 'https://stashpedia.com' + link
+    main_page = requests.get(link)
+    main_page = main_page.text
+    number = '#'+str(re.findall('<H5>#(.*?)<', main_page)[0])
+    stash_price = re.findall('<span class="gridValue">(.*?)<', search_page)[0]
+    title = re.findall('<H4 class="toUpperCase">(.*?)<', search_page)[0]
+    image_link = 'https://stashpedia.com'+re.findall('img-responsive gridImage" src="(.*?)"', search_page)[0]
+    return stash_price, title, number, image_link, link
+
+
+def ebay(search):
+    link = 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313.TR12.TRC2.A0.H0.XTEST.TRS0&_nkw=%s' \
+           '&_sacat=0&LH_TitleDesc=0&_osacat=0&_odkw=%%23101' % search
+    search_page = requests.get(link)
+    search_page = search_page.text
+    price = re.findall('<span class="s-item__price">(.*?)<', search_page)[0]
+    return price
+
+
+def main(name):
+    stash_price, title, number, image_link, link = stash(name)
+    ebay_search = (title+' '+number).replace(' ', '+').replace('#', '%23')
+    ebay_price = ebay(ebay_search)
+
+    embed = discord.Embed(title=title, url=link, description="", color=0x00ff00)
+    embed.add_field(name="Ebay Price", value=ebay_price, inline=False)
+    embed.add_field(name="Stashpedia Price", value=stash_price, inline=False)
+    embed.set_image(url=image_link)
+
+    return embed
+
 
 @bot.event
 async def on_ready():
@@ -70,13 +52,14 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+
 @bot.event
 async def on_message(message):
-   if message.content.startswith('!funko'):
-       name=(message.content).split(' ')[1:]
-       name=' '.join(name)
-       embed=main(name)
-       await bot.send_message(message.channel, embed=embed)
+    if message.content.startswith('!funko'):
+        name = message.content.split(' ')[1:]
+        name = ' '.join(name)
+        embed = main(name)
+        await bot.send_message(message.channel, embed=embed)
 
 
 bot.run(TOKEN)
